d45d28a97d0ac75b3d4aa7b2ed15e54d9cc90bcf
Azerino Yogananta Gatot Subroto <azerino25@gmail.com>
2019-01-23 23:38:57 +0700

    add .env.default and dotenv

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config
+
+module.exports = {
+  development: {
+    username: process.env.DB_DEV_USERNAME,
+    password: process.env.DB_DEV_PASSWORD,
+    database: process.env.DB_DEV_NAME,
+    host: process.env.DB_DEV_HOST,
+    dialect: process.env.DB_DEV_DIALECT
+  },
+  test: {
+    username: process.env.DB_TEST_USERNAME,
+    password: process.env.DB_TEST_PASSWORD,
+    database: process.env.DB_TEST_NAME,
+    host: process.env.DB_TEST_HOST,
+    dialect: process.env.DB_TEST_DIALECT
+  },
+  production: {
+    username: process.env.DB_PRODUCTION_USERNAME,
+    password: process.env.DB_PRODUCTION_PASSWORD,
+    database: process.env.DB_PRODUCTION_NAME,
+    host: process.env.DB_PRODUCTION_HOST,
+    dialect: process.env.DB_PRODUCTION_DIALECT
+  }
+};
